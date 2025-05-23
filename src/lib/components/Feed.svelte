c08935e8e716928a976eafd6f4eaadfc56b2386d
LeGrizzly <xalsie.ff@hotmail.fr>
2025-05-23 04:10:13 +0200

    Refactor Feed component to include loading state with CardSkeleton; remove unused Card component code

--- src/lib/components/Feed.svelte
+++ src/lib/components/Feed.svelte
@@ -1,10 +1,18 @@
 <script>
-	import { onMount } from 'svelte';
 	import Card from './card/Card.svelte';
+  import CardSkeleton from './card/Card.Skeleton.svelte';
+	export let feed = [];
+	export let loadingFeed = false;
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-  {#each Array(39) as _, index}
-    <Card />
-  {/each}
+  {#if loadingFeed}
+		{#each Array(6) as _, i}
+			<CardSkeleton />
+		{/each}
+	{:else}
+    {#each Array(39) as _, index}
+      <Card />
+    {/each}
+	{/if}
 </div>
